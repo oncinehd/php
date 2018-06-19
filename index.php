8591450116608aaa0fee2ed54149debf3bd23b97
oncinehd <oncinehd@gmail.com>
2018-06-19 14:01:27 -0300

    resolvi o clonfito 2

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-<<<<<<< HEAD
-    echo "Olá Mundo! 456";
-    echo "Olá Udemy";
-=======
-    echo "Olá Mundo! 123";
 
-    echo "Olá Hacode";
->>>>>>> c5df0e4c7a33e89b7b5f313302c37b4d967c536f
+    echo "Olá Mundo! 456";
+    echo "Olá Udemys";
+
 ?>
